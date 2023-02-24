57e451b2d000a6c22603029811112bef0619e86f
Vinay Goud <92563613+vinaygoud253@users.noreply.github.com>
2023-02-24 22:15:10 +0530

    Add files via upload

---
+++ Weatherapp.js
@@ -0,0 +1,55 @@
+let result = document.getElementById("result");
+let searchBtn = document.getElementById("search-btn");
+let cityRef = document.getElementById("city");
+
+//Function to fetch weather details from api and display them
+let getWeather = () => {
+  let cityValue = cityRef.value;
+  let key ="fb93433fdac64852aa2d2a6834150822";
+  //If input field is empty
+  if (cityValue.length == 0) {
+    result.innerHTML = `<h3 class="msg">Please enter a city name</h3>`;
+  }
+  //If input field is NOT empty
+  else {
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${cityValue}&appid=${key}`;
+    //Clear the input field
+    // https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+    cityRef.value = "";
+    fetch(url)
+      .then((resp) => resp.json())
+      //If city name is valid
+      .then((data) => {
+        console.log(data);
+        console.log(data.weather[0].icon);
+        console.log(data.weather[0].main);
+        console.log(data.weather[0].description);
+        console.log(data.name);
+        console.log(data.main.humidity);
+        console.log(data.wind.speed);
+        result.innerHTML = `
+        <h2>${data.name}</h2>
+        <h4 class="weather">${data.weather[0].main}</h4>
+        <h4 class="desc">${data.weather[0].description}</h4>
+        <img src="https://openweathermap.org/img/w/${data.weather[0].icon}.png">
+        <h1>${data.main.temp} &#176;</h1>
+        <div class="temp-container">
+            <div>
+                <h4 class="title">Humidity</h4>
+                <h4 class="temp">${data.main.humidity}%;</h4>
+            </div>
+            <div>
+                <h4 class="title">Wind Speed</h4>
+                 <h4 class="temp">${data.wind.speed}km/hr;</h4>
+            </div>
+        </div>
+        `;
+      })
+      //If city name is NOT valid
+      .catch(() => {
+        result.innerHTML = `<h3 class="msg">City not found</h3>`;
+      });
+  }
+};
+searchBtn.addEventListener("click", getWeather);
+// window.addEventListener("load", getWeather);
